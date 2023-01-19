617afc805243304393fd0e585d1539b3eb129f03
Miguel Gonzalez <maglez@gmail.com>
2023-01-19 00:03:27 +0000

    Request and Response examples

---
+++ Demo/04_request.py
@@ -0,0 +1,12 @@
+### Running application: uvicorn 04_request:app
+
+from starlette.requests import Request
+from starlette.responses import Response
+
+# http://127.0.0.1:8000
+async def app(scope, receive, send):
+    assert scope['type'] == 'http'
+    request  = Request(scope, receive)
+    content = f"{request.method} - {request.url.path}"
+    response = Response(content, media_type= 'text/plain')
+    await response(scope, receive, send)
